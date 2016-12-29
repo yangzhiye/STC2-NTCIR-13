851b9ddf2794084c7db2bd736009aa8035c485ac
Zhiye Yang <445922703@qq.com>
2016-12-29 19:20:49 +0800

    re to clean data

--- get_data.py
+++ get_data.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
-__author__ = 'vino'
+#import jieba
+import re
 
-import jieba
 
 def cut_word(filepath):
+
+    def _remove_special_char(m):
+        s = m.group(0)
+        if s in u'，。！？“”《》':
+            return s
+        return ''
+	
     f = open(filepath)
     for line in f.readlines():
-        result = line.split("\t")[-1]
-        result = jieba.cut(result)
-        print ' '.join(result)
+        content = line.split("\t")[-1].decode('utf-8')
+	content = re.sub(u'[\(\[（#「【].*[\)\]）#」】]', '', content)
+	content = re.sub(u'[^\u4e00-\u9fa50-9a-zA-Z]', _remove_special_char, content).encode('utf-8')
+	print content
+       # result = jieba.cut(result)
+       # print ' '.join(result)
 
 
 if __name__ == "__main__":
 
     filepath = "./data/repository/stc2-repos-id-post"
+    #filepath = "./data/repository/stc2-repos-id-cmnt"
     cut_word(filepath)
+
