d6b63c8c5e03aafc93046d26090d43348779e128
Zhiye Yang <445922703@qq.com>
2017-02-22 16:27:00 +0800

    update

--- test_keras_preprocessing.py
+++ test_keras_preprocessing.py
@@ -4,11 +4,11 @@ from keras.preprocessing import text,sequence
 
 def test(filepath):
 
-	
+
 
 
 if __name__ == "__main__":
-	
+
     filepath_list = ["./data/repository/stc2-repos-id-post",
                      "./data/repository/stc2-repos-id-cmnt"]
     for filepath in filepath_list:
